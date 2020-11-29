6787f5e61569a809d2852d6386540a0330de812f
Nevermind <nvmefh@gmail.com>
2020-11-29 20:57:22 -0300

    Complete sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+	if (!Number.isInteger(n1) | !Number.isInteger(n2) | (n1 < 0) | (n2 < 0)) {
+		return "ERROR";
+	}
 
-}
+	if (n2 > n1) {
+		if (n2 === n1) {
+			return n2++;
+		} else {
+			return n2 + sumAll(n1, n2 - 1);
+		}
+	} else {
+		if (n1 === n2) {
+			return n1++;
+		} else {
+			return n1 + sumAll(n1 - 1, n2);
+		}
+	}
+};
 
-module.exports = sumAll
+module.exports = sumAll;
