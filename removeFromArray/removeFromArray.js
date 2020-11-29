752acb1042417040e8b5e01c25d648199dd1ec02
Nevermind <nvmefh@gmail.com>
2020-11-29 20:01:34 -0300

    Complete removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+	return arr.filter((x) => !args.includes(x));
+};
 
-}
-
-module.exports = removeFromArray
+module.exports = removeFromArray;
