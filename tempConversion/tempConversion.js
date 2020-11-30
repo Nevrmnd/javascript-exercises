3786bdd89672c5cfc3c13f7d3b68c78a03df0e9a
Nevermind <nvmefh@gmail.com>
2020-11-29 21:33:41 -0300

    Complete tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
-const ftoc = function() {
+const ftoc = function (temp) {
+	const converted = (temp - 32) * (5 / 9);
+	return Number.isInteger(converted) ? converted : +converted.toFixed(1);
+};
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function (temp) {
+	const converted = temp * (9 / 5) + 32;
+	return Number.isInteger(converted) ? converted : +converted.toFixed(1);
+};
 
 module.exports = {
-  ftoc,
-  ctof
-}
+	ftoc,
+	ctof,
+};
