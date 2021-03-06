c2d35bef01c08bf8e854e089948b198339d83442
Nevermind <nvmefh@gmail.com>
2021-03-06 04:19:00 -0300

    Add solution to fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,3 +1,19 @@
-const fibonacci = function (num) {};
+const fibonacci = function (number) {
+	const index = parseInt(number);
+	if (index < 0) {
+		return "OOPS";
+	}
+	const arr = [1];
+	for (let i = 0; i < index; i++) {
+		if (arr.length === 1) {
+			arr.push(1);
+		} else {
+			let n = arr[i] + arr[i - 1];
+			arr.push(n);
+		}
+	}
+
+	return arr[index - 1];
+};
 
 module.exports = fibonacci;
